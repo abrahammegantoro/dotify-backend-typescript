05f733bcf192ee1991ce0ffd0404fda5f0e835e8
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2024-02-17 13:10:31 +0700

    [FEAT] Create users and posts

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Post } from 'src/posts/entities/post.entity';
+import { PrimaryGeneratedColumn, Column, Entity, OneToMany } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+}
